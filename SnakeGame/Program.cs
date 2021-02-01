20e2f072a560811c037bb76cab1317eef84e113f
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-02-01 08:33:28 +0200

    .

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SnakeGame
 {
@@ -6,7 +10,48 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(80, 25);
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                System.Threading.Thread.Sleep(100);
+
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                System.Threading.Thread.Sleep(100);
+                snake.Move();
+            }
+            
+
         }
     }
 }
