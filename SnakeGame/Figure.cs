ecbe23632d82420e9991706f41296f922b5202a8
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-02-07 16:00:37 +0200

    //

--- SnakeGame/Figure.cs
+++ SnakeGame/Figure.cs
@@ -6,34 +6,37 @@ using System.Threading.Tasks;
 
 namespace SnakeGame
 {
-    class Figure
-    {
-        protected List<Point> pList;
+	class Figure
+	{
+		protected List<Point> pList;
 
-        public void Draw()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
-        }
-        internal bool IsHit (Figure figure)
-        {
-            foreach(var p in pList)
-            {
-                if (figure.IsHit(p))
-                    return true;
-            }
-            return false;
-        }
-        private bool IsHit(Point point)
-        {
-            foreach(var p in pList)
-            {
-                if (p.IsHit(point))
-                    return true;
-            }
-            return false;
-        }
-    }
+		public void Draw()
+		{
+			foreach (Point p in pList)
+			{
+				p.Draw();
+			}
+		}
+
+		internal bool IsHit(Figure figure)
+		{
+			foreach (var p in pList)
+			{
+				if (figure.IsHit(p))
+					return true;
+			}
+			return false;
+		}
+
+		private bool IsHit(Point point)
+		{
+			foreach (var p in pList)
+			{
+				if (p.IsHit(point))
+					return true;
+			}
+			return false;
+		}
+	}
 }
+
--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -2,56 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace SnakeGame
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.SetWindowSize(80, 25);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(80, 25);
 
-            Walls walls = new Walls(80, 25);
-            walls.Draw();
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
-            //Отрисовка точек
-            Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
 
-            while (true)
-            {
-                if (walls.IsHit(snake) || snake.IsHitTail())
-                {
-                    break;
-                }
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    snake.Move();
-                }
-                System.Threading.Thread.Sleep(100);
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			Console.ReadLine();
+		}
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
 
-                if(Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
-                System.Threading.Thread.Sleep(100);
-                snake.Move();
-            }
-            
-
-        }
-    }
+	}
 }
